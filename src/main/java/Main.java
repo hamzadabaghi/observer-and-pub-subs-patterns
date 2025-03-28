75892c5a782590455a0c4993a53f5c943893e26d
hamzadabaghi <hamza.dabaghi@gmail.com>
2025-03-28 15:35:02 +0000

    Refactor the subject class in the observer pattern

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,37 +9,36 @@ import pub_sub.example.publishers.UserService;
 import pub_sub.example.subscribers.AnalyticsService;
 import pub_sub.example.subscribers.EmailService;
 
-import java.lang.ref.WeakReference;
-
 public class Main {
 
-    private static String TOPIC = "USER_REGISTRATION";
+    private static String BROKER_TOPIC = "USER_REGISTRATION";
+
     public static void main(String[] args) {
 
         /* ------ observer pattern : architecture ------ */
 
-//        System.out.println("\n------ observer pattern : architecture ------\n");
-//        ConcreteSubject subject = new ConcreteSubject();
-//        WeakReference<Observer> observer1 = new WeakReference<>(new ConcreteObserver());
-//        WeakReference<Observer> observer2 = new WeakReference<>(new ConcreteObserver());
-//        subject.attach(observer1);
-//        subject.attach(observer2);
-//        subject.setState(10);
-//        subject.setState(20);
+        System.out.println("\n------ observer pattern : architecture ------\n");
+        ConcreteSubject subject = new ConcreteSubject();
+        Observer observer1 = new ConcreteObserver();
+        Observer observer2 = new ConcreteObserver();
+        var weakReferenceObserver1 = subject.attach(observer1);
+        var weakReferenceObserver2 = subject.attach(observer2);
+        subject.setState(10);
+        subject.setState(20);
 
 
         /* ------ observer pattern : example ------ */
 
-//        System.out.println("\n------ observer pattern : example ------\n");
-//        Stocks stockDatasource = new Stocks(5);
-//        WeakReference<Observer> statusBarObserver = new WeakReference<>(new StatuBar(stockDatasource));
-//        WeakReference<Observer> statusListViewObserver = new WeakReference<>(new StatusListView(stockDatasource));
-//        stockDatasource.attach(statusBarObserver);
-//        stockDatasource.attach(statusListViewObserver);
-//        stockDatasource.addStock(5);
-//        stockDatasource.addStock(10);
-//        stockDatasource.addStock(20);
-//        stockDatasource.updateStockPrice(0, 30);
+        System.out.println("\n------ observer pattern : example ------\n");
+        Stocks stockDatasource = new Stocks(5);
+        Observer statusBarObserver = new StatuBar(stockDatasource);
+        Observer statusListViewObserver = new StatusListView(stockDatasource);
+        var weakstockDatasourceReference = stockDatasource.attach(statusBarObserver);
+        var weakStatusListViewReference = stockDatasource.attach(statusListViewObserver);
+        stockDatasource.addStock(5);
+        stockDatasource.addStock(10);
+        stockDatasource.addStock(20);
+        stockDatasource.updateStockPrice(0, 30);
 
 
 
@@ -50,8 +49,8 @@ public class Main {
         MessageBroker messageBroker = new MessageBroker();
         AnalyticsService analyticsService = new AnalyticsService();
         EmailService emailService = new EmailService();
-        messageBroker.subscribe(TOPIC, analyticsService);
-        messageBroker.subscribe(TOPIC, emailService);
+        messageBroker.subscribe(BROKER_TOPIC, analyticsService);
+        messageBroker.subscribe(BROKER_TOPIC, emailService);
         UserService userService = new UserService(messageBroker);
         userService.registerUser("hamza", "hamza.dabaghi@gmail.com");
     }
--- src/main/java/observer/architecture/Subject.java
+++ src/main/java/observer/architecture/Subject.java
@@ -3,23 +3,27 @@ package observer.architecture;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public abstract class Subject {
     private final List<WeakReference<Observer>> observers = new ArrayList<>();
-    public void attach(WeakReference<Observer> observer) {
-        observers.add(observer);
+
+    public WeakReference<Observer> attach(Observer observer) {
+        WeakReference<Observer> weakObserver = new WeakReference<>(observer);
+        observers.add(weakObserver);
+        return weakObserver;
     }
+
     public void detach(WeakReference<Observer> observer) {
         observers.remove(observer);
     }
+
     public void notifyObservers() {
         for (WeakReference<Observer> observer : observers) {
-            Observer o = observer.get();
-            if (o != null) {
-                o.update();
-            } else {
-                observers.remove(observer);
-            }
+            Optional.ofNullable(observer.get()).ifPresentOrElse(
+                    Observer::update,
+                    () -> detach(observer)
+            );
         }
     }
 }
